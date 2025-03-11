6969517194ace20731782f4024c9b40820aa2bd5
pavan <pavankumar10036@gmail.com>
2025-03-11 22:43:54 +0530

    model

--- src/main/java/com/example/demo/model/ProductModel.java
+++ src/main/java/com/example/demo/model/ProductModel.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
     private String productName;
     private String productDes;
